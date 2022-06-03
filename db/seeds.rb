3488fcbf2bcdf7c5016e002ac6e4ec2875076caa
Laxmi-Pasi <laxmip.@simformsolutions.com>
2022-06-03 13:54:40 +0530

    feat: created workshop model and added data inside that

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "workshops are being created"
+
+workshops = Workshop.create!([{
+    name:"Full stack Ruby on Rails Development Bootcamp",
+    description: "Some dummy project.",
+    start_date: Date.today + 2.days,
+    end_date: Date.today + 20.days,
+    start_time: "10:00 AM",
+    end_time: "3:00 AM",
+    total_sits: 100,
+    remaining_sits: 0,
+    registration_fee: 100
+},
+{
+    name:"Python",
+    description: "Some dummy project.",
+    start_date: Date.today + 12.days,
+    end_date: Date.today + 20.days,
+    start_time: "10:00 AM",
+    end_time: "3:00 AM",
+    total_sits: 100,
+    remaining_sits: 0,
+    registration_fee: 1000
+},
+{
+    name:"Advance Java",
+    description: "Some dummy project.",
+    start_date: Date.today + 2.days,
+    end_date: Date.today + 20.days,
+    start_time: "10:00 AM",
+    end_time: "3:00 AM",
+    total_sits: 100,
+    remaining_sits: 0,
+    registration_fee: 1200
+}])
+puts "workshops created"
