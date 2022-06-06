2286de5899c61c5e5bb3ecfde037ed12b6028c83
Laxmi-Pasi <laxmip.@simformsolutions.com>
2022-06-06 20:32:26 +0530

    feat:added functionality in stripe services

--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -4,6 +4,7 @@ class StripeService
     Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
   end
 
+  #to generate stripe id for particular customer
   def find_or_create_customer(customer)
     if customer.stripe_customer_id.present?
       stripe_customer = Stripe::Customer.retrieve(customer.stripe_customer_id)
@@ -16,5 +17,38 @@ class StripeService
       customer.update(stripe_customer_id: stripe_customer.id)
     end
     stripe_customer
+  end
+
+  #to generate card token
+
+  def create_card_token(params)
+    Stripe::Token.create({
+      card: {
+        number: params[:card_number].to_s,
+        exp_month: params[:month],
+        exp_year: params[:year],
+        cvc: params[:cvv],
+      },
+    })
+  end
+
+  #to generate token and create card for customer
+  def create_stripe_customer_card(params, stripe_customer)
+    token = create_card_token(params)
+    Stripe::Customer.create_source(
+      stripe_customer.id,
+      {source: token.id }
+    )
+  end
+
+  #create stripe charge
+  def create_stripe_charge(amount_to_be_paid, stripe_customer_id, card_id, workshop)
+    Stripe::Charge.create({
+      amount: amount_to_be_paid,
+      currency: 'usd',
+      source: card_id,
+      customer: stripe_customer_id,
+      description: "Amount $#{amount_to_be_paid} charged for #{workshop.name}",
+    })
   end
 end
