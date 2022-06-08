adf55a080c65f2863630a370a8a686833f1ee50e
Laxmi-Pasi <laxmip.@simformsolutions.com>
2022-06-08 11:56:55 +0530

    feat:added order number to booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,9 +2,17 @@ require "rqrcode"
 class Booking < ApplicationRecord
   belongs_to :customer
   belongs_to :workshop
+
+  validates :order_number, presence: true, uniqueness: true
+
+  before_validation :generate_order_number
   after_create_commit :update_workshop_sit_count
 
   def update_workshop_sit_count
     workshop.update(remaining_seats: workshop.remaining_seats - no_of_tickets)
+  end
+
+  def generate_order_number
+    self.order_number = "BOOKING-#{SecureRandom.hex(5).upcase}"
   end
 end
